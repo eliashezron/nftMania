5db6cde6cc21718166b4ba20f926769c91ea3c50
eliashezron <eliashezron23@gmail.com>
2022-10-30 12:27:59 +0000

    add

--- NFTContract.sol
+++ NFTContract.sol
@@ -6,21 +6,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract DeveloperApes is ERC721, Ownable {
+contract NFTMania is ERC721, Ownable {
     using Counters for Counters.Counter;
     string public _baseURL;
-    uint256 public mintPrice = 20; //USD
-    
+    uint public mintPrice;
 
     Counters.Counter private _tokenIdCounter;
 
     constructor(
-    ) ERC721("DevApes", "DVA") {
+    ) ERC721("NFTMANIA", "NFTM") {
     
     }
 
     function Mint() external payable {
-        require(msg.value >= mintingFee(), "Insufficient funds in your wallet");
         uint256 tokenId = _tokenIdCounter.current();
         _mint(msg.sender, tokenId);
         _tokenIdCounter.increment();
@@ -35,17 +33,7 @@ contract DeveloperApes is ERC721, Ownable {
     function setPrice(uint256 _price) public onlyOwner {
     mintPrice = _price;
     }
-    function getLatestPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        // for eth usd, the price is scale up by 10**8
-        return uint256(price / 1e8);
-    }
-    function mintingFee() public view returns(uint256){
-        uint256 price = getLatestPrice();
-        uint256 mintEthPrice = (mintPrice*10**18)/price;
-        return mintEthPrice;
-    }
+
     function withdraw() external onlyOwner {
     uint balance = address(this).balance;
     payable(owner()).transfer(balance);
